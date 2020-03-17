7218f5eea558aec96b9eecfc81f6b7e45930880d
Demcic <51235240+Demcic@users.noreply.github.com>
2020-03-17 18:52:36 +0100

    Add files via upload

--- mini-test/zadatak 1/index.php
+++ mini-test/zadatak 1/index.php
@@ -1,5 +1,6 @@
 <?php 
-
+//imao sam za ideju da uporedim da li je koriscenje switch/case naredbe brze od if naredbi
+//rezultati na mom lokalnom hostu kazu da je switch/case malo brze od if naredbi
 
 $time_start = microtime_float();
 for ($i=1; $i <= 100; $i++) { 
